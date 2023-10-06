ccccc12f22c87b58b7b1e5feb675573c50d46de7
luiscar05 <penaabellaluiscarlos@gmail.com>
2023-10-06 10:49:18 -0500

    descarga cors

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from "cors"
 import usuarioRoute from './routers/usuario.route.js';
 import juegoRoute from './routers/juego.route.js';
 import AlquilerRouter from './routers/alquiler.route.js';
@@ -8,6 +9,8 @@ const app =express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(cors())
 
 app.set('view engine','ejs');
 app.set('views', './views');
